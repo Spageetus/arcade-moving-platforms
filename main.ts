26659abfaaf364b20a6b0fe598607666a7c98f4a
Spageetus <95888955+Spageetus@users.noreply.github.com>
2024-11-02 17:49:24 -0400

    Platform overlap code now functions

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 namespace SpriteKind {
-    export let Platform = SpriteKind.create()
-    for (let sprite of sprites.allOfKind(9999)) {
-        sprite.setKind(SpriteKind.Platform)
-    }
+    export let Platform = 9999
 }
-
 namespace MoveablePlatform
 {
+    let isOnPlatform = false
+    let currentPlatform: Sprite = null
+    
     export function create(img: Image) {
         if (SpriteKind.Platform == undefined) { //Platform kind is undefined when this function runs for the first time
             SpriteKind.Platform = 9999
@@ -16,7 +15,24 @@ namespace MoveablePlatform
 
     export function platformCollisionHandler(sprite: Sprite, platform: Sprite)
     {
-        
+        if (sprite.right < platform.x) {
+            sprite.right = platform.left
+        }
+        else if (sprite.left > platform.x) {
+            sprite.left = platform.right
+        }
+        else if (sprite.bottom <= platform.y) //hits top of platform
+        {
+            isOnPlatform = true
+            currentPlatform = platform
+            sprite.ay = 0
+            sprite.vy = 0
+            sprite.bottom = platform.top
+        }
+        else if (sprite.top >= platform.y) //hit bottom of tile
+        {
+            sprite.top = platform.bottom
+            sprite.vy = 0
+        }
     }
 }
-
--- test.ts
+++ test.ts
@@ -1,2 +1,24 @@
 let p1 = MoveablePlatform.create(assets.image`platform1x1`)
-console.log(p1.kind())
+tiles.setCurrentTilemap(assets.tilemap`testLevel`)
+scene.setBackgroundColor(9)
+
+let playerSprite = sprites.create(assets.image`playerImage`, SpriteKind.Player)
+tiles.placeOnTile(playerSprite, tiles.getTileLocation(0, 0))
+playerSprite.ay = 600
+controller.moveSprite(playerSprite, 100, 0)
+
+let platforms = sprites.allOfKind(SpriteKind.Platform)
+console.log(SpriteKind.Platform)
+
+controller.up.onEvent(ControllerButtonEvent.Pressed, function()
+{
+    if(playerSprite.isHittingTile(CollisionDirection.Bottom))
+    {
+        playerSprite.vy = -250
+    }
+})
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Platform, function(sprite: Sprite, platform: Sprite)
+{
+    MoveablePlatform.platformCollisionHandler(sprite, platform)
+})
